6521993e16746021d0fd2c3549a5c4b433c3bc2f
Radika <86754890+Radhika4035@users.noreply.github.com>
2022-03-23 18:42:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload()
 {
@@ -12,6 +16,29 @@ function setup ()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.om('pose', gotPoses);
+}
+
+function gotPoses()
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " +  leftWristY);
+    
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " +  rightWristY);
+    }
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
 }
 
 function draw()
@@ -22,4 +49,6 @@ function draw()
 function play() 
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
